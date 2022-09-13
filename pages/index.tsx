393d58c13819a7b1fcd8ff1fa564df7fea269a63
Ignacio Ferro Picón <nacho@binpar.com>
2022-09-13 10:42:55 +0200

    fix: unpoched

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
-import { useTranslation } from 'next-i18next';
 
-const Index: React.FC = () => {
-  const { t } = useTranslation('index');
-  return (
-    <>
-      <Head>
-        <title>Ejemplo de página de EMP en TW</title>
-        <meta name="description" content="Ejemplo de página de EMP en TW" />
-        <meta property="og:image" content="/assets/rocketLaptop.png" />
-      </Head>
-      <h1>Ejemplo de página de EMP en TW</h1>
-    </>
-  );
-};
+const Index: React.FC = () => (
+  <>
+    <Head>
+      <title>Ejemplo de página de EMP en TW</title>
+      <meta name="description" content="Ejemplo de página de EMP en TW" />
+      <meta property="og:image" content="/assets/rocketLaptop.png" />
+    </Head>
+    <h1 className="text-primary text-5xl">Ejemplo de página de EMP en TW</h1>
+  </>
+);
 
 export default Index;
