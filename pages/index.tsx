7b6c8e1f7d9ab2bee5807ddeffef4a091db385b9
Adrian Rodriguez <adrian@binpar.com>
2022-09-15 09:28:13 +0200

    chore: working in header

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,58 @@ const Index: React.FC = () => (
       <meta name="description" content="Ejemplo de página de EMP en TW" />
       <meta property="og:image" content="/assets/rocketLaptop.png" />
     </Head>
-    <h1 className="text-primary text-5xl">Ejemplo de página de EMP en TW</h1>
+    <div className="bg-primaryvariant h-[70px] text-white text-center tracking-wider flex flex-col justify-center">
+      <p className="">Semana de la Fisioterapia, hasta 50% dto.</p>
+      <div className="mt-2 text-xs font-light flex justify-center">
+        <p>Promoción válida hasta el 15 de septiembre.</p>
+        <a
+          href="https://www.medicapanamericana.com/es/general/formacion/profesional-fisioterapeuta"
+          className="uppercase pb-1 border-b border-white hover:bg-white"
+        >
+          Ver cursos.
+        </a>
+      </div>
+    </div>
+    <header>
+      <div className="bg-lightgray">
+        <div className="flex flex-row items-center justify-end text-secondarygray text-2xs uppercase h-[35px] tracking-widest w-[calc(_100%_-_100px)] m-auto">
+          <div>
+            <a href="/es/somos">Editorial</a>
+            <a className="ml-2.5 pl-2.5 border-l border-secondarygray" href="/es/autores">
+              Autores
+            </a>
+            <a
+              href="https://aula.campuspanamericana.com/"
+              className="ml-2.5 pl-2.5 border-l border-primary"
+            >
+              Campus
+            </a>
+          </div>
+          <div className="text-primary uppercase tracking-widest ml-8 flex">
+            <button type="button" className="uppercase tracking-widest">
+              Iniciar sesión
+            </button>
+            <a
+              className="ml-2.5 pl-2.5 border-l-2 border-primary"
+              rel="nofollow"
+              href="/es/registrarme"
+            >
+              Registrarse
+            </a>
+            <a
+              className="ml-2.5 pl-2.5 pr-2.5 border-l-2 border-r-2 border-primary"
+              rel="nofollow"
+              href="/es/contacto"
+            >
+              Contacto
+            </a>
+            <div className="inline-block pl-2.5">
+              <img src="/img/spain.png" alt="es" className="aspect-square w-4 rounded-xl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   </>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#0074A7',
+        primary: '#0074a7',
+        primaryvariant: '#196696',
         secondary: '#eedb4c',
+        lightgray: '#f9f9f9',
+        secondarygray: '#7a94a4'
       },
       screens: {
       },
       fontFamily: {
+        'sans': ['Muli', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '2xs': ['0.6rem', {
+          letterSpacing: '1.15',
+        }],
       }
     },
   },
