9d2f4597c4dfa428d70f702052e5c745e69a911d
Adrián <monocreations@gmail.com>
2023-02-10 11:33:01 +0100

    chore: product ratings !CU-865bpxz6c

--- src/components/sections/SectionSelection.tsx
+++ src/components/sections/SectionSelection.tsx
@@ -1,16 +1,21 @@
+import FullStar from '../icons/FullStar';
+import EmptyStar from '../icons/StarEmpty';
+import ButtonStar from './items/ButonStar';
 import ProductBuy from './items/ProductBuy';
 import ProductMessage from './items/ProductMessage';
 import ProductPrice from './items/ProductPrice';
 import ProductSelection from './items/ProductSelection';
 import ProductType from './items/ProductType';
+import ProductValoration from './items/ProductValoration';
 
 const SectionSelection: React.FC = () => (
-  <section className="max-w-[250px] absolute right-0 border-l-2 border-[#f9f9f9]">
+  <section className="max-w-[250px] xl:max-w-[310px] absolute right-0 border-l-2 border-[#f9f9f9]">
     <ProductSelection />
     <ProductType />
     <ProductMessage />
     <ProductPrice />
     <ProductBuy />
+    <ProductValoration />
   </section>
 );
 export default SectionSelection;
