7efb4c8722126eac042037574fa2e3e44d3cb93e
Adrian Rodriguez <adrian@binpar.com>
2023-03-28 17:56:06 +0200

    chore: login form !CU-865c131rz

---
+++ src/components/sections/checkOut/RegisterForm.tsx
@@ -0,0 +1,9 @@
+const RegisterForm: React.FC = () => (
+  <>
+    <p className="uppercase text-gray-500 text-sm tracking-[1px] font-light pb-2 border-b border-gray-300">
+      ACCEDE A TU CUENTA
+    </p>
+    
+  </>
+);
+export default RegisterForm;
